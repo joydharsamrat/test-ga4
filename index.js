e9d37f6810e2b0009555fa07ac986e2431ae3aa4
joydharsamrat <joydharsamrat@gmail.com>
2024-04-08 11:15:50 +0600

    initialized

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { BetaAnalyticsDataClient } = require("@google-analytics/data");
+const { GoogleAuth } = require("google-auth-library");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOptions));
+
+const auth = new GoogleAuth({
+  keyFile: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+  scopes: "https://www.googleapis.com/auth/analytics.readonly",
+});
+
+const analyticsDataClient = new BetaAnalyticsDataClient({ auth });
+
+// Rest of your code remains unchanged
+const propertyId = 435060008;
+
+async function runReport() {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: "2024-03-31",
+        endDate: "today",
+      },
+    ],
+    dimensions: [
+      {
+        name: "pagePath",
+      },
+    ],
+    metrics: [
+      {
+        name: "screenPageViews",
+      },
+    ],
+  });
+
+  console.log("Report result:");
+  response.rows.forEach((row) => {
+    console.log(row.dimensionValues[0], row.metricValues[0]);
+  });
+  return response;
+}
+
+app.get("/", (req, res) => {
+  const result = runReport();
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
